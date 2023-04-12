16bf2e732067957b94e76f93bca59432eb3ed121
Hany Juliana <64250079+hanysitio98@users.noreply.github.com>
2023-04-12 15:17:11 +0700

    Initial commit

---
+++ src/main/java/id/co/bca/spring/funtravelpayment/FunTravelPayment/FunTravelPaymentApplication.java
@@ -0,0 +1,13 @@
+package id.co.bca.spring.funtravelpayment.FunTravelPayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunTravelPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunTravelPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/bca/spring/funtravelpayment/FunTravelPayment/FunTravelPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.bca.spring.funtravelpayment.FunTravelPayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunTravelPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
