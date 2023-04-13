0517305fe533289af7f66980881c3aea77e6cc99
Hany Juliana <64250079+hanysitio98@users.noreply.github.com>
2023-04-13 16:00:39 +0700

    update readme

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Payment Service
+
+## 2023-12-04
+- [Adjustment]
+    - add model for payment service
+    - update billing repo and service
+
+## 2023-13-04
+- [Adjustment]
+    - add service and controller
+    - update payment method
--- src/main/java/id/co/bca/spring/funtravelpayment/FunTravelPayment/service/BillingServiceSDJ.java
+++ src/main/java/id/co/bca/spring/funtravelpayment/FunTravelPayment/service/BillingServiceSDJ.java
@@ -4,9 +4,11 @@ import id.co.bca.spring.funtravelpayment.FunTravelPayment.model.Billing;
 import id.co.bca.spring.funtravelpayment.FunTravelPayment.repository.BillingSDJRepository;
 import id.co.bca.spring.funtravelpayment.FunTravelPayment.repository.IBillingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class BillingServiceSDJ {
 
     @Autowired
